04765bad25ab5bf69b27c22a5c8190f8e1b83179
SEUNGJAE-97 <117517535+SEUNGJAE-97@users.noreply.github.com>
2022-11-12 21:23:16 +0900

    11.12(오후)

--- shape_example.py
+++ shape_example.py
@@ -1,59 +1,44 @@
-import matplotlib.pyplot as plt
 import cv2
-import numpy as np
 
-target = cv2.imread('drug1.png')
-shapes = cv2.imread('shape1.png')
-
-def image_processing(image, shape):
-    # target
-    # 출력 할 빈 캔버스 
-    output = np.zeros((image.shape[0],image.shape[1],3), np.uint8)
-    # 그레이, 바이너리 스케일 변환 
-    targetGray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    shapesGray = cv2.cvtColor(shape, cv2.COLOR_BGR2GRAY)
-
-    targetTh = cv2.adaptiveThreshold(targetGray, 255, cv2.ADAPTIVE_THRESH_MEAN_C  , cv2.THRESH_BINARY, 11, 1)
+def find_contr(path, path2):
+    # 이미지 처리 
+    target= cv2.imread(path2)
+    shapes = cv2.imread(path)
+    shapesGray = cv2.cvtColor(shapes, cv2.COLOR_BGR2GRAY)
+    targetGray = cv2.cvtColor(target, cv2.COLOR_BGR2GRAY)
     shapesTh = cv2.adaptiveThreshold(shapesGray, 255, cv2.ADAPTIVE_THRESH_MEAN_C  , cv2.THRESH_BINARY, 11, 1)
-    
-    # find contours 
-    cntrs_target,_= cv2.findContours(targetTh,cv2.RETR_TREE,cv2.CHAIN_APPROX_NONE) 
-    cntrs_shapes,_= cv2.findContours(shapesTh,cv2.RETR_TREE,cv2.CHAIN_APPROX_NONE) 
-    
-    matchs = [] # 컨투어와 매칭 점수를 보관할 리스트
-    
-    # thresh(1000)보다 작은 contours는 제거한다. 
-    saved_contour_shapes = []
+    targetTh = cv2.Canny(targetGray, 150, 250)
+    cv2.imshow('targetTh',targetTh)
+    # 모든 컨투어 찾기 
+    cntrs_shapes, hierarchy = cv2.findContours(shapesTh,cv2.RETR_TREE,cv2.CHAIN_APPROX_NONE) 
+    cntrs_target, hierarchy = cv2.findContours(targetTh,cv2.RETR_TREE,cv2.CHAIN_APPROX_NONE)
+    # 노이즈 제거 
     saved_contour_target = []
-    thresh = 1000
-    
-    for contour in cntrs_target:
-        if cv2.contourArea(contour) > thresh:
-            saved_contour_target.append(contour)
+    saved_contour_shapes = []
+    matchs = []
+    thresh_low = 100
+    thresh_high = 1000
 
     for contoured in cntrs_shapes:
-        if cv2.contourArea(contoured) > thresh:
+        if (cv2.contourArea(contoured) > thresh_low) & (cv2.contourArea(contoured) < thresh_high):
             saved_contour_shapes.append(contoured)
-    # 저장된 saved_contour_shapes를 output에 그린다.
-    cv2.drawContours(output, saved_contour_shapes,-1,(255,255,255),1)
-
-    for contr in cntrs_shapes:
-        # 대상 도형과 여러 도형 중 하나와 매칭 실행 
-        match = cv2.matchShapes(cntrs_target[0], contr, cv2.CONTOURS_MATCH_I2, 0.0)
-        # 해당 도형의 매칭 점수와 컨투어를 쌍으로 저장 
-        matchs.append( (match, contr) )
-        # 해당 도형의 컨투어 시작지점에 매칭 점수 표시 
-        cv2.putText(shapes, '%.2f'%match, tuple(contr[0][0]), cv2.FONT_HERSHEY_PLAIN, 0,(255,0,0),3 )
-    # 매칭 점수로 정렬 
-    matchs.sort(key=lambda x : x[0])
-    # 가장 target에 근사한 값을 갖는 도형의 컨투어 표시
-    cv2.drawContours(shapes, [matchs[0][1]],-1, (0,255,0),3)
+    for contour in cntrs_target:
+        if (cv2.contourArea(contour) > thresh_low) :
+            saved_contour_target.append(contour)
+            
+    for contr in saved_contour_shapes:
+        match = cv2.matchShapes(saved_contour_target[0], contr, cv2.CONTOURS_MATCH_I2, 0.0)
+        matchs.append((match,contr))
+        cv2.putText(shapes, '%.2f'%match, tuple(contr[0][0]), cv2.FONT_HERSHEY_PLAIN, 1, (0,0,255),1)
     
-    # 결과 출력
-    fig = plt.figure(figsize=(10, 6))
-    plt.subplot(121), plt.axis('off'), plt.imshow(output), plt.title("target")
-    plt.subplot(122), plt.axis('off'), plt.imshow(shapes), plt.title("Match Shape")
-    plt.show()
+    matchs.sort(key = lambda x : x[0])
+    cv2.drawContours(shapes, [matchs[0][1]], -1, (0,255,0), 3)
     
+    print(len(saved_contour_shapes))
 
-image_processing(target, shapes)
+    cv2.imshow("result", shapes)
+    cv2.waitKey(0)
+
+path2 = 'drug1.png'
+path = 'shape.jpg'
+find_contr(path, path2)
