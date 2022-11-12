8b46bab8cb7a7399b45d1134d46983f5061c9cc5
SEUNGJAE-97 <117517535+SEUNGJAE-97@users.noreply.github.com>
2022-11-12 21:34:43 +0900

    find_shape(완료)

--- shape_example.py
+++ shape_example.py
@@ -8,12 +8,12 @@ def find_contr(path, path2):
     targetGray = cv2.cvtColor(target, cv2.COLOR_BGR2GRAY)
     shapesTh = cv2.adaptiveThreshold(shapesGray, 255, cv2.ADAPTIVE_THRESH_MEAN_C  , cv2.THRESH_BINARY, 11, 1)
     targetTh = cv2.Canny(targetGray, 150, 250)
-    cv2.imshow('targetTh',targetTh)
+    
     # 모든 컨투어 찾기 
     cntrs_shapes, hierarchy = cv2.findContours(shapesTh,cv2.RETR_TREE,cv2.CHAIN_APPROX_NONE) 
     cntrs_target, hierarchy = cv2.findContours(targetTh,cv2.RETR_TREE,cv2.CHAIN_APPROX_NONE)
+    
     # 노이즈 제거 
-    saved_contour_target = []
     saved_contour_shapes = []
     matchs = []
     thresh_low = 100
@@ -22,19 +22,22 @@ def find_contr(path, path2):
     for contoured in cntrs_shapes:
         if (cv2.contourArea(contoured) > thresh_low) & (cv2.contourArea(contoured) < thresh_high):
             saved_contour_shapes.append(contoured)
+    
+    """
+    # 제거 
     for contour in cntrs_target:
-        if (cv2.contourArea(contour) > thresh_low) :
+        if (cv2.contourArea(contour) > 50) :
             saved_contour_target.append(contour)
-            
+    """
+    
+          
     for contr in saved_contour_shapes:
-        match = cv2.matchShapes(saved_contour_target[0], contr, cv2.CONTOURS_MATCH_I2, 0.0)
+        match = cv2.matchShapes(cntrs_target[0], contr, cv2.CONTOURS_MATCH_I2, 0.0)
         matchs.append((match,contr))
         cv2.putText(shapes, '%.2f'%match, tuple(contr[0][0]), cv2.FONT_HERSHEY_PLAIN, 1, (0,0,255),1)
     
     matchs.sort(key = lambda x : x[0])
     cv2.drawContours(shapes, [matchs[0][1]], -1, (0,255,0), 3)
-    
-    print(len(saved_contour_shapes))
 
     cv2.imshow("result", shapes)
     cv2.waitKey(0)
