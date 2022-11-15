c82fb4e85f86788aef9557324d2f78f37028e98c
SEUNGJAE-97 <117517535+SEUNGJAE-97@users.noreply.github.com>
2022-11-15 19:50:53 +0900

    11-15
    엎고 다시 처음부터 할 것....

--- main.py
+++ main.py
@@ -1,4 +1,21 @@
 import cv2
-import shape_color
 
 
+path = 'drug.PNG'
+target = cv2.imread(path)
+targetGray = cv2.cvtColor(target, cv2.COLOR_BGR2GRAY)
+targetTh = cv2.adaptiveThreshold(targetGray, 255, cv2.ADAPTIVE_THRESH_MEAN_C  , cv2.THRESH_BINARY, 11, 1)
+
+cntrs_shapes, hierarchy = cv2.findContours(targetTh,cv2.RETR_TREE,cv2.CHAIN_APPROX_NONE)
+
+saved_contour_shapes = []
+thresh_low = 500
+thresh_high = 1000
+
+for contoured in cntrs_shapes:
+        if (cv2.contourArea(contoured) > thresh_low) & (cv2.contourArea(contoured) < thresh_high):
+            saved_contour_shapes.append(contoured)
+
+cv2.drawContours(target, saved_contour_shapes, -1, (255,255,0), 3)
+cv2.imshow('target',target)
+cv2.waitKey(0)
